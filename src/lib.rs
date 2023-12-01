a70955ec8e91ae447ec1c6b2bc70153cae80d145
fesmjke <fatalerror.sm@gmail.com>
2023-12-01 21:27:50 +0200

    feat: added tests for day1 solution part a, and part b

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+#[cfg(test)]
+mod tests {
+
+    mod day_1 {
+        use day1;
+
+        #[test]
+        fn small_a() {
+            let value = day1::calibration("./day1/input_small_a.txt");
+
+            assert_eq!(value, 142);
+        }
+
+        #[test]
+        fn small_b() {
+            let value = day1::calibration("./day1/input_small_b.txt");
+
+            assert_eq!(value, 281);
+        }
+
+        #[test]
+        fn input() {
+            let a = day1::calibration("./day1/input_ab.txt");
+
+            println!("{:?}", a);
+
+            assert!(a >= 0)
+        }
+    }
+}
